8639de013f5722c8757acfe21b88b5f62c9109a3
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-03-17 02:35:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,54 +18,40 @@ from pccm.extension import ExtCallback, PCCMBuild, PCCMExtension
 from setuptools import Command, find_packages, setup
 from setuptools.extension import Extension
 
-# Package meta-data.
 NAME = 'cumm'
 RELEASE_NAME = NAME
 cuda_ver = os.getenv("CUMM_CUDA_VERSION", None)
-
 if cuda_ver is not None and cuda_ver != "":
-    cuda_ver = cuda_ver.replace(".", "")  # 10.2 to 102
+    cuda_ver = cuda_ver.replace(".", "")
     RELEASE_NAME += "-cu{}".format(cuda_ver)
 
-DESCRIPTION = 'CUda Matrix Multiply library'
-URL = 'https://github.com/FindDefinition/cumm'
-EMAIL = 'yanyan.sub@outlook.com'
-AUTHOR = 'Yan Yan'
-REQUIRES_PYTHON = '>=3.8'
+DESCRIPTION = 'CUDA matrix multiply library'
+URL = 'https://github.com/rathaROG/cumm-gpu'
+AUTHOR = 'rathaROG'
+REQUIRES_PYTHON = '>=3.11'
 VERSION = None
 
-# What packages are required for this module to be executed?
 REQUIRED = [
     "pccm>=0.4.15",
     "pybind11>=2.6.0",
     "fire",
     "numpy",
     "sympy",
-    "contextvars; python_version == \"3.6\"",
 ]
 
-# What packages are optional?
 EXTRAS = {
     # 'fancy feature': ['django'],
 }
 
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(str(Path(__file__).parent))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
     with open('version.txt', 'r') as f:
@@ -73,7 +59,6 @@ if not VERSION:
 else:
     version = VERSION
 cwd = os.path.dirname(os.path.abspath(__file__))
-
 
 def _convert_build_number(build_number):
     parts = build_number.split(".")
@@ -84,7 +69,6 @@ def _convert_build_number(build_number):
     else:
         raise NotImplementedError
 
-
 env_suffix = os.environ.get("CUMM_VERSION_SUFFIX", "")
 if env_suffix != "":
     version += ".dev{}".format(_convert_build_number(env_suffix))
@@ -94,10 +78,8 @@ about['__version__'] = version
 with open(version_path, 'w') as f:
     f.write("__version__ = '{}'\n".format(version))
 
-
 class UploadCommand(Command):
     """Support setup.py upload."""
-
     description = 'Build and publish the package.'
     user_options = []
 
@@ -105,33 +87,25 @@ class UploadCommand(Command):
     def status(s):
         """Prints things in bold."""
         print('\033[1m{0}\033[0m'.format(s))
-
     def initialize_options(self):
         pass
-
     def finalize_options(self):
         pass
-
     def run(self):
         try:
             self.status('Removing previous builds...')
             rmtree(os.path.join(here, 'dist'))
         except OSError:
             pass
-
         self.status('Building Source and Wheel (universal) distribution...')
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(
             sys.executable))
-
         self.status('Uploading the package to PyPI via Twine...')
         os.system('twine upload dist/*')
-
         self.status('Pushing git tags...')
         os.system('git tag v{0}'.format(about['__version__']))
         os.system('git push --tags')
-
         sys.exit()
-
 
 class CopyHeaderCallback(ExtCallback):
     def __call__(self, ext: Extension, package_dir: Path,
@@ -168,7 +142,6 @@ class CopyHeaderCallback(ExtCallback):
             # shutil.copytree(nvrtc_headers, target_dir)
 
 disable_jit = os.getenv("CUMM_DISABLE_JIT", None)
-
 if disable_jit is not None and disable_jit == "1":
     cmdclass = {
         'upload': UploadCommand,
@@ -188,12 +161,9 @@ if disable_jit is not None and disable_jit == "1":
                       std="c++17" if compat.InMacOS else "c++14",)
     ]
 else:
-    cmdclass = {
-        'upload': UploadCommand,
-    }
+    cmdclass = {'upload': UploadCommand,}
     ext_modules = []
 
-# Where the magic happens:
 setup(
     name=RELEASE_NAME,
     version=about['__version__'],
@@ -201,23 +171,17 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     author=AUTHOR,
-    author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=('tests', )),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
     entry_points={
         'console_scripts': [],
     },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
+    license='Apache-2.0',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: CPython',
